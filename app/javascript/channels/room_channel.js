810e706f03d45bb717fc9c8faaced4b8d8a51c69
Joseph Hung <arixxle@gmail.com>
2020-05-12 23:21:48 +0800

    切換聊天室 但turbolinks無法順利運作 會讓聊天室重複訂閱

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,30 +1,38 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 2}, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log('connected...');
-  },
+document.addEventListener('turbolinks:load', () => {
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  const room_element = document.querySelector('#room-id')
+  const room_id = Number(room_element.getAttribute('data-room-id'));
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log(data);
-
-    const element = document.querySelector('#user-id')
-    const user_id = Number(element.getAttribute('data-user-id'));
-
-    let html;
-    if( user_id === data.message.user_id ) {
-      html = data.mine;
-    } else {
-      html = data.theirs;
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id}, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log('connected...' + room_id);
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log(data);
+  
+      const user_element = document.querySelector('#user-id')
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+  
+      let html;
+      if( user_id === data.message.user_id ) {
+        html = data.mine;
+      } else {
+        html = data.theirs;
+      }
+  
+      const messageContainer = document.querySelector('#message')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
     }
+  });
+})
 
-    const messageContainer = document.querySelector('#message')
-    messageContainer.innerHTML = messageContainer.innerHTML + html
-  }
-});
+
