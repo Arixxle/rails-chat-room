6a731412ea3a3ad48d4c9e39ca36c78351c219ce
Joseph Hung <arixxle@gmail.com>
2020-05-12 23:01:35 +0800

    區分mine theirs message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,13 +27,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.user = current_user
     @message.save
-    # redirect_to request.referrer
-    html = render(
-      partial: 'messages/message', 
-      locals: { message: @message }
-    )
-
-    ActionCable.server.broadcast "room_channel_#{@message.room_id}", html: html
+    SendMessageJob.perform_later(@message)
   end
 
   # PATCH/PUT /messages/1
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 1}, {
+consumer.subscriptions.create({ channel: "RoomChannel", room_id: 2}, {
   connected() {
     // Called when the subscription is ready for use on the server
     console.log('connected...');
@@ -13,5 +13,18 @@ consumer.subscriptions.create({ channel: "RoomChannel", room_id: 1}, {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
     console.log(data);
+
+    const element = document.querySelector('#user-id')
+    const user_id = Number(element.getAttribute('data-user-id'));
+
+    let html;
+    if( user_id === data.message.user_id ) {
+      html = data.mine;
+    } else {
+      html = data.theirs;
+    }
+
+    const messageContainer = document.querySelector('#message')
+    messageContainer.innerHTML = messageContainer.innerHTML + html
   }
 });
